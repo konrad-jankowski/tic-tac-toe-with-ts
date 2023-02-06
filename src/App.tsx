6e3f087db9bce7e45bd3d3d617dbca95cff8ee3a
blackweeed <blackweeeed@gmail.com>
2023-02-06 14:36:02 +0100

    make user able to make a move

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 import Square from "./Square";
 
-const INITIAL_GAME_STATE = ["X", "O", "", "", "", "", "", "", ""];
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  const handleClick = (e: any) => {
+    const cellIndex = Number(e.target.getAttribute("data-cell-index"));
+    const currValue = gameState[cellIndex];
+    if (currValue) {
+      return;
+    }
+    // const newValues = gameState.map((item) => item);
+    const newValues = [...gameState];
+    newValues[cellIndex] = currentPlayer;
+    setGameState(newValues);
+    console.log(newValues);
+  };
+
   return (
     <div className="h-full  p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-white text-5xl mb-4 text-center">Tic tac toe</h1>
       <div>
         <div className="grid grid-cols-3 gap-3 mx-auto w-96">
           {gameState.map((player, index) => (
-            <Square key={index} {...{ player }} />
+            <Square
+              key={index}
+              handleClick={handleClick}
+              {...{ player, index }}
+            />
           ))}
         </div>
         <div>Scores Go Here</div>
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,14 +1,25 @@
 type Props = {
   player: string;
+  index: number;
+  handleClick: (e: any) => void;
 };
 
-function Square({ player }: Props) {
+function Square({ player, index, handleClick }: Props) {
   const scale = player ? "scale-100" : "scale-0";
   const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300";
   const hoverStyle = "transition duration-500 hover:scale-105 transform";
   return (
-    <div className="h-36 border-solid border-4 border-slate-200 text-7xl text-center flex justify-center item-center items-center cursor-pointer ">
-      {player}
+    <div
+      data-cell-index={index}
+      className={`h-36 border-solid border-4 border-slate-200 text-7xl text-center flex justify-center item-center items-center cursor-pointer ${hoverStyle}`}
+      onClick={handleClick}
+    >
+      <span
+        data-cell-index={index}
+        className={`transform transition-none duration-150 ease-out ${scale} ${textColor}`}
+      >
+        {player}
+      </span>
     </div>
   );
 }
