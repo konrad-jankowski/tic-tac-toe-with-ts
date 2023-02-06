cd08e733b43444a07ee68f7e0648869d09f71968
blackweeed <blackweeeed@gmail.com>
2023-02-06 15:10:34 +0100

    add winner,draft,board reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "./Square";
 
 const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
 
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  useEffect(() => {
+    checkForWinner();
+  }, [gameState]);
+
+  const checkForWinner = () => {
+    let roundWon = false;
+    const resetBoard = () => setGameState(INITIAL_GAME_STATE);
+
+    for (let i = 0; i < WINNING_COMBOS.length; i++) {
+      const winCombo = WINNING_COMBOS[i];
+
+      let a = gameState[winCombo[0]];
+      let b = gameState[winCombo[1]];
+      let c = gameState[winCombo[2]];
+
+      if ([a, b, c].includes("")) {
+        continue;
+      }
+
+      if (a === b && b === c) {
+        roundWon = true;
+        break;
+      }
+    }
+
+    if (roundWon) {
+      setTimeout(() => {
+        window.alert(`Congrats player ${currentPlayer}! You are the winner`);
+        resetBoard();
+      }, 500);
+
+      return;
+    }
+
+    if (!gameState.includes("")) {
+      setTimeout(() => {
+        window.alert("The game ended in a draw");
+        resetBoard();
+      }, 500);
+
+      return;
+    }
+
+    changePlayer();
+  };
+
+  const changePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
 
   const handleClick = (e: any) => {
     const cellIndex = Number(e.target.getAttribute("data-cell-index"));
@@ -13,11 +74,9 @@ function App() {
     if (currValue) {
       return;
     }
-    // const newValues = gameState.map((item) => item);
     const newValues = [...gameState];
     newValues[cellIndex] = currentPlayer;
     setGameState(newValues);
-    console.log(newValues);
   };
 
   return (
